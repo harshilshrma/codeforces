927b196e7fd90eee416f133357339a906cefcb77
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-08-12 12:10:53 +0530

    first commit

---
+++ ChewbaccaAndNumber/Main.java
@@ -0,0 +1,26 @@
+package ChewbaccaAndNumber;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String num = sc.next();
+        StringBuilder str = new StringBuilder();
+
+        for (int i = 0; i < num.length(); i++) {
+            int digit = num.charAt(i) - '0';
+            int inverted = 9 - digit;
+
+            if (inverted < digit && !(i == 0 && inverted == 0)) {
+                str.append(inverted);
+            } else {
+                str.append(digit);
+            }
+        }
+
+        System.out.println(Integer.parseInt(str.toString()));
+    }
+
+}
+
---
+++ bit++/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = 0;
+
+        for (int i = 0; i < n; i++) {
+            String statment = sc.next();
+            if (statment.contains("++")) {
+                x++;
+            } else {
+                x--;
+            }
+        }
+
+        System.out.println(x);
+        sc.close(); 
+    }
+}
