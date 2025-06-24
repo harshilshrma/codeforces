f2f6f19cc9f2a2db034584c6d3ea3e84920d3bb7
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-06-24 12:11:01 +0530

    naming convention changed

--- BeautifulMatrix.java
+++ BeautifulMatrix.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class beautifulmatrix {
+public class BeautifulMatrix {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[][] mat = new int[5][5];
