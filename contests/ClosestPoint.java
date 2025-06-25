ee0700369f6b88662c80d399e612341a63386228
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-06-25 14:04:20 +0530

    minor

--- contests/ClosestPoint.java
+++ contests/ClosestPoint.java
@@ -1,5 +1,6 @@
+package contests;
 import java.util.*;
-public class CONTESTClosestPoint {
+public class ClosestPoint {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
