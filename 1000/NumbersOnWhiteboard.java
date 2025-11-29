6ecb2913b2a3f068e05c1dac89037ea0fc0d67aa
Harshil Sharma <harshilsharma.dev@gmail.com>
2025-11-29 23:23:15 +0530

    1000C - numbers on whiteboard

--- 1000/NumbersOnWhiteboard.java
+++ 1000/NumbersOnWhiteboard.java
@@ -1,10 +1,46 @@
-import java.util.Scanner;
-public class NumbersOnWhiteboard {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
+import java.io.*;
+import java.util.ArrayList;
 
+public class NumbersOnWhiteboard {
+    static class Pair {
+        int a;
+        int b;
+
+        public Pair(int a, int b) {
+            this.a = a;
+            this.b = b;
+        }
+    }
+    private static void solve(BufferedReader br, BufferedWriter bw) throws IOException {
+        int n = Integer.parseInt(br.readLine());
         
-        sc.close();
+        bw.write("2\n");
+
+        ArrayList<Pair> list = new ArrayList<>();
+
+        int curr = n;
+        for (int i = n - 1; i >= 1; i--) {
+            list.add(new Pair(curr, i));
+            curr = (curr + i + 1) / 2;
+        }
+
+        for (Pair p : list) {
+            bw.write(p.a + " " + p.b + "\n");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            solve(br, bw);
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
